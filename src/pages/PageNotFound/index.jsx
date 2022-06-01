1e715c153672684ae3f34158594c751f08acdac4
Bao <baothaitruong2000@gmail.com>
2022-06-02 00:16:46 +0700

    new repo

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function PageNotFound() {
     return (
         <div class="pageNotFound">
-            <p>haha</p>
             <div className='pageNotFound__img'>
                 <img src="https://images.unsplash.com/photo-1633078654544-61b3455b9161?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=745&q=80"></img>
             </div>
