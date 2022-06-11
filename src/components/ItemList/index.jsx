a9593e2b65a5d68bdc73a2202cc3bf5a2b795607
Bao <baothaitruong2000@gmail.com>
2022-06-11 18:51:33 +0700

    update linked more

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -5,7 +5,7 @@ import './style.scss';
 export default function ItemList({ children, address, mainLink }) {
     return (
         <div className='itemList'>
-            <Link to={address === 'home' ? '/' : mainLink ? '/' + mainLink + '/' + address : address}><span>{children}</span>
+            <Link to={address === 'home' ? '/CFD-ReactJs' : mainLink ? '/' + mainLink + '/' + address : address}><span>{children}</span>
                 <div><ArrowDown /></div>
             </Link >
         </div>
